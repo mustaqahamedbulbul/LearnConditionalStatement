b6f26bdab679a3fd2d1c1779eb3a110b9d90d788
mustaqahamedbulbul <mustaqbulbul1999@gmail.com>
2021-10-24 05:16:37 +0530

    Initial commit:Basic structure and simple shape classes. Tests don't pass.

---
+++ src/com/mustaqahamedbulbul/Main.java
@@ -0,0 +1,23 @@
+package com.mustaqahamedbulbul;
+
+/**
+ * Author : MAB
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+    //Print your branch name
+        String brunch = "CSE";
+        if (brunch == "CSE") {
+            System.out.println("Computer Science and Engineering");
+         }else if (brunch == "Electronics") {
+            System.out.println("Electronics Communication and Engineering");
+        } else if (brunch == "Electrical") {
+            System.out.println("Electrical and Engineering");
+        } else {
+            System.out.println("Invalid Branch");
+        }
+    }
+
+}
